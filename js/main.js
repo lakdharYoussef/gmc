0481e2eeb99d5ec5fbcdca3df4d0a8ceda962fdb
lakdharYoussef <57589410+lakdharYoussef@users.noreply.github.com>
2020-01-14 09:03:55 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ document.getElementById("gras").addEventListener("click",function(){
     else{
         target.style.fontWeight="bolder"
  } 
- })
+  })
  document.getElementById("ital").addEventListener("click",function(){
     if(target.style.fontStyle=="italic"){
     target.style.fontStyle="normal";
@@ -30,3 +30,95 @@ document.getElementById("gras").addEventListener("click",function(){
  document.getElementById("font").addEventListener("change",function(){
     target.style.fontSize=document.getElementById("font").value
 })
+$(".course1").hover(function(){
+    $(".btnCourses1").toggleClass('btnCourses1Hover')
+})
+
+$(".course2").hover(function(){
+    $(".btnCourses2").toggleClass('btnCourses1Hover')
+})
+
+$(".course3").hover(function(){
+    $(".btnCourses3").toggleClass('btnCourses1Hover')
+})
+
+$(".course4").hover(function(){
+    $(".btnCourses4").toggleClass('btnCourses1Hover')
+})
+
+$(".course5").hover(function(){
+    $(".btnCourses5").toggleClass('btnCourses1Hover')
+})
+
+$(".course6").hover(function(){
+    $(".btnCourses6").toggleClass('btnCourses1Hover')
+})
+
+$(".course7").hover(function(){
+    $(".btnCourses7").toggleClass('btnCourses1Hover')
+})
+
+$(".course8").hover(function(){
+    $(".btnCourses8").toggleClass('btnCourses1Hover')
+})
+
+$(".course9").hover(function(){
+    $(".btnCourses9").toggleClass('btnCourses1Hover')
+})
+
+
+$(".open").on("click", function(){
+    $(".popup, .popup-content").addClass("active");
+    });
+
+ $(".close, .popup").on("click", function(){
+ $(".popup, .popup-content").removeClass("active");
+});
+
+ $(".course1").click(function(){
+$("#message").modal({
+ fadeDuration: 100
+});
+ })
+        
+ $(".course2").click(function(){
+$("#message").modal({
+ fadeDuration: 100
+});
+ })
+
+ $(".course3").click(function(){
+  $("#message").modal({
+  fadeDuration: 100
+ });
+})
+  $(".course4").click(function(){
+$("#message").modal({
+  fadeDuration: 100
+ });
+ })
+    $(".course5").click(function(){
+     $("#message").modal({
+     fadeDuration: 100
+     });
+     })
+   $(".course6").click(function(){
+       $("#message").modal({
+        fadeDuration: 100
+      });
+      })
+    $(".course7").click(function(){
+  $("#message").modal({
+  fadeDuration: 100
+  }) 
+  })
+ $(".course8").click(function(){
+   $("#message").modal({
+ fadeDuration: 100
+  });
+  })
+  $(".course9").click(function(){
+     $("#message").modal({
+          fadeDuration: 100
+       });
+      })
